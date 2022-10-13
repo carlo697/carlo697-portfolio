2a2af5634259208a8060a9cc7c6465e557fed5a9
carlo697 <carlos.07888@gmail.com>
2022-10-13 01:02:56 -0400

    Add SimpleDivider component

---
+++ components/separators/SimpleDivider.tsx
@@ -0,0 +1,37 @@
+import classNames from "classnames";
+import React from "react";
+import styles from "./SimpleDivider.module.scss";
+
+type Props = {
+  containerClassName?: string;
+  dividerClassName?: string;
+  direction?: "vertical" | "horizontal";
+  useContainer?: boolean;
+};
+
+const SimpleDivider = ({
+  containerClassName,
+  dividerClassName,
+  direction = "horizontal",
+  useContainer = false,
+}: Props) => {
+  const containerClasses = classNames("container", dividerClassName);
+  const dividirClasses = classNames(
+    styles["base"],
+    direction === "horizontal" ? styles["horizontal"] : styles["vertical"]
+  );
+
+  const content = <div className={dividirClasses}></div>;
+
+  return (
+    <>
+      {useContainer ? (
+        <div className={containerClasses}>{content}</div>
+      ) : (
+        content
+      )}
+    </>
+  );
+};
+
+export default SimpleDivider;
