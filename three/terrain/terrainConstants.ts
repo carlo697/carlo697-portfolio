1cebd78c471f3933cbac688d4f455370b7d4021d
carlo697 <carlos.07888@gmail.com>
2022-11-10 22:55:09 -0400

    Add multiple seeds to the terrain

--- three/terrain/terrainConstants.ts
+++ three/terrain/terrainConstants.ts
@@ -1,4 +1,6 @@
-export const terrainSeed = 1005;
+const seeds = [1005, 1, 2, 3, 4, 7, 9, 12, 13, 14, 15, 16, 17, 20];
+export const terrainSeed = seeds[Math.floor(Math.random() * seeds.length)];
+// export const terrainSeed = 1005;
 
 export const terrainTotalSize = 100;
 export const terrainChunkDivisions = 4;
