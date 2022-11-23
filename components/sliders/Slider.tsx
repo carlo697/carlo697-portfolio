4ed2f75e301c193a5d2dee180657d6556a5a0a19
carlo697 <carlos.07888@gmail.com>
2022-11-23 00:58:17 -0400

    Add Slider component

---
+++ components/sliders/Slider.tsx
@@ -0,0 +1,107 @@
+import Image, { StaticImageData } from "next/image";
+import React, { useState } from "react";
+import styles from "./Slider.module.scss";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import classNames from "classnames";
+
+export type SliderImage = {
+  src: string | StaticImageData;
+  alt: string;
+};
+
+export type SliderProps = {
+  images: Array<SliderImage>;
+};
+
+const Slider = ({ images }: React.PropsWithoutRef<SliderProps>) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const setIndex = (index: number) => {
+    if (index > images.length - 1) {
+      setCurrentSlide(0);
+    } else if (index < 0) {
+      setCurrentSlide(images.length - 1);
+    } else {
+      setCurrentSlide(index);
+    }
+  };
+
+  const enableButtons = images.length > 1;
+
+  return (
+    <div>
+      <div className={styles["first-row"]}>
+        {enableButtons && (
+          <button
+            className={styles["arrow-wrapper"]}
+            onClick={() => setIndex(currentSlide - 1)}
+          >
+            <BsChevronCompactLeft />
+          </button>
+        )}
+
+        <div className={styles["slides-wrapper"]}>
+          {images.map(({ src, alt }, index) => {
+            const className =
+              index < currentSlide
+                ? styles["slide-left"]
+                : index > currentSlide
+                ? styles["slide-right"]
+                : styles["slide-active"];
+
+            return (
+              <div
+                className={classNames(styles["img-wrapper"], className)}
+                key={index}
+              >
+                <Image
+                  src={src}
+                  alt={alt}
+                  placeholder="blur"
+                  className="hola"
+                  objectFit="contain"
+                />
+              </div>
+            );
+          })}
+        </div>
+
+        {enableButtons && (
+          <button
+            className={styles["arrow-wrapper"]}
+            onClick={() => setIndex(currentSlide + 1)}
+          >
+            <BsChevronCompactRight />
+          </button>
+        )}
+      </div>
+
+      {enableButtons && (
+        <div className={styles["previews"]}>
+          {images.map(({ src, alt }, index) => {
+            return (
+              <button
+                className={classNames(
+                  styles["img-preview-wrapper"],
+                  index === currentSlide && styles["active"]
+                )}
+                key={index}
+                onClick={() => setCurrentSlide(index)}
+              >
+                <Image
+                  src={src}
+                  alt={alt}
+                  placeholder="blur"
+                  className="hola"
+                  objectFit="contain"
+                />
+              </button>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Slider;
